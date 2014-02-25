064c4d5f882940cfc243b5c96b84ced510bed8c7
raxyu <xuanyu1@yahoo.com>
2014-02-25 12:14:41 -0500

    test pagination on get_all files from the vault

--- deuce/tests/test_files_controller.py
+++ deuce/tests/test_files_controller.py
@@ -4,6 +4,7 @@ import hashlib
 import string
 from random import randrange
 import six
+from six.moves.urllib.parse import urlparse, parse_qs
 from unittest import TestCase
 from deuce.tests import FunctionalTest
 from deuce.drivers.storage.metadata.sqlite import SqliteStorageDriver
@@ -14,6 +15,9 @@ class TestFilesController(FunctionalTest):
     def setUp(self):
         super(TestFilesController, self).setUp()
 
+        self.file_list = []
+        self.max_ret_num = conf.api_configuration.max_returned_num
+        self.total_file_num = 0
         self._hdrs = {"X-Project-ID": "test_project_id"}
 
         # Create a vault and a file for us to work with
@@ -27,7 +31,7 @@ class TestFilesController(FunctionalTest):
         # Create File
         response = self.app.post(self._files_path, headers=self._hdrs)
         self._file_id = response.headers["Location"]
-        self._file_id = self._file_id.replace('http://localhost', '')
+        self._file_id = urlparse(self._file_id).path
         # Now, _file_id is '/v1.0/files_vault_test/files/SOME_FILE_ID'
 
         self._NOT_EXIST_files_path = '/v1.0/not_exists/files'
@@ -45,6 +49,94 @@ class TestFilesController(FunctionalTest):
             headers=self._hdrs, expect_errors=True)
 
         assert response.status_int == 404  # Not found
+
+    def helper_create_files(self, num):
+        params = {}
+        for cnt in range(0, num):
+            response = self.app.post(self._files_path, headers=self._hdrs)
+            file_id = response.headers["Location"]
+            response = self.app.post(file_id,
+                params=params, headers=self._hdrs)
+            file_id = urlparse(file_id).path.split('/')[-1]
+            self.file_list.append(file_id)
+        return num
+
+    def helper_get_files(self, marker, limit, assert_return_url,
+            assert_data_len, repeat=False):
+        resp_file_list = []
+        if limit != 0:
+            params = {'marker': marker, 'limit': limit}
+        else:
+            params = {'marker': marker}
+        while True:
+            response = self.app.get(self._files_path,
+                params=params, headers=self._hdrs)
+            next_batch_url = response.headers["X-Next-Batch"]
+            tmp_file_list = list(response.json_body)
+            resp_file_list = resp_file_list + tmp_file_list
+
+            if not repeat:
+                assert (not next_batch_url) == (not assert_return_url)
+                assert len(resp_file_list) == assert_data_len
+                for h in resp_file_list:
+                    assert h in self.file_list
+                if assert_data_len == -1 or \
+                        assert_data_len == self.total_file_num:
+                    for h in self.file_list:
+                        assert h in resp_file_list
+                return next_batch_url
+            if not next_batch_url:
+                break
+            params['marker'] = \
+                parse_qs(urlparse(next_batch_url).query)['marker']
+        assert len(resp_file_list) == assert_data_len
+        for h in resp_file_list:
+            assert h in self.file_list
+        for h in self.file_list:
+            assert h in resp_file_list
+
+    def test_get_all(self):
+        # Create few (< max_returned_num) files in the vault
+        file_num = int(0.5 * self.max_ret_num)
+        self.total_file_num = self.helper_create_files(file_num)
+
+        # Get list of files in the vault with a given limit.
+        next_batch_url = self.helper_get_files(marker=0, limit=4,
+                assert_return_url=True, assert_data_len=4)
+
+        # Get list of all files in the vault with the default limit.
+        self.helper_get_files(marker=0, limit=0, assert_return_url=False,
+                assert_data_len=self.total_file_num)
+
+        # Add more (< max_returned_num) files in the vault
+        file_num = int(0.5 * self.max_ret_num) - 1
+        self.total_file_num += self.helper_create_files(file_num)
+
+        # Get list of all files in the vault.
+        self.helper_get_files(marker=0, limit=0, assert_return_url=False,
+                assert_data_len=self.total_file_num)
+
+        # Add one more file
+        self.total_file_num += self.helper_create_files(1)
+
+        # Get list of all files in the vault, which is exact one load.
+        next_batch_url = self.helper_get_files(marker=0, limit=0,
+                assert_return_url=True,
+                assert_data_len=self.total_file_num)
+
+        # Get list of all files in the vault from last run,
+        # and returns nothing.
+        marker = parse_qs(urlparse(next_batch_url).query)['marker']
+        next_batch_url = self.helper_get_files(marker=marker,
+                limit=0, assert_return_url=False, assert_data_len=0)
+
+        # Add more files to make the total files more than one load
+        file_num = int(0.5 * self.max_ret_num)
+        self.total_file_num += self.helper_create_files(file_num)
+
+        # Get list of all files in the vault, need multiple runs,
+        self.helper_get_files(marker=0, limit=0, assert_return_url=False,
+                assert_data_len=self.total_file_num, repeat=True)
 
     def test_get_one(self):
         # vault does not exists
