d7773d516ee38e6e27dcc7987bd63b70dc7646b4
raxyu <xuanyu1@yahoo.com>
2014-03-20 12:28:52 -0400

    1. create tmp testing mongod server and tear it down before exit.  2. unittest does self cleanup before exit, 3. pass db conn to driver.

--- deuce/tests/test_mongodb_storage_driver.py
+++ deuce/tests/test_mongodb_storage_driver.py
@@ -1,5 +1,14 @@
 from pecan import conf
 
+import pymongo
+from pymongo import MongoClient
+import tempfile
+import subprocess
+import os
+import time
+import atexit
+import shutil
+
 from deuce.tests import FunctionalTest
 from deuce.drivers.storage.metadata import MetadataStorageDriver
 from deuce.drivers.storage.metadata.mongodb import MongoDbStorageDriver
@@ -7,16 +16,38 @@ from deuce.drivers.storage.metadata.mongodb import MongoDbStorageDriver
 
 class MongoDbStorageDriverTest(FunctionalTest):
 
+    def __init__(self, *args, **kwargs):
+        self.db = MongoTemporaryInstance.get_instance()
+        self.conn = self.db.conn
+        super(MongoDbStorageDriverTest, self).__init__(*args, **kwargs)
+        atexit.register(self.shutdown)
+
+    def shutdown(self):
+        if os.path.exists('/tmp/deuce_mongo_unittest_vaultmeta.db'):\
+                #pragma no cover
+            os.remove('/tmp/deuce_mongo_unittest_vaultmeta.db')\
+                #pragma no cover
+
+    def setUp(self):
+        super(MongoDbStorageDriverTest, self).setUp()
+
+        for db_name in self.conn.database_names():
+            self.conn.drop_database(db_name)
+
     def test_basic_construction(self):
-        driver = MongoDbStorageDriver()
+        try:
+            # Try system existed mongod
+            driver = MongoDbStorageDriver()
+        except:  # pragma no cover
+            assert True
 
     def test_geneology(self):
-        driver = MongoDbStorageDriver()
+        driver = MongoDbStorageDriver(self.conn)
         assert isinstance(driver, MetadataStorageDriver)
         assert isinstance(driver, object)
 
     def test_file_crud(self):
-        driver = MongoDbStorageDriver()
+        driver = MongoDbStorageDriver(self.conn)
 
         project_id = 'project_id'
         vault_id = 'vault_id'
@@ -37,7 +68,7 @@ class MongoDbStorageDriverTest(FunctionalTest):
         files = driver.create_file_generator(project_id, vault_id)
 
     def test_finalize_empty_file(self):
-        driver = MongoDbStorageDriver()
+        driver = MongoDbStorageDriver(self.conn)
 
         project_id = 'project_id'
         vault_id = 'vault_id'
@@ -54,7 +85,7 @@ class MongoDbStorageDriverTest(FunctionalTest):
         assert driver.is_finalized(project_id, vault_id, file_id)
 
     def test_finalize_nonexistent_file(self):
-        driver = MongoDbStorageDriver()
+        driver = MongoDbStorageDriver(self.conn)
 
         project_id = 'project_id'
         vault_id = 'vault_id'
@@ -72,7 +103,7 @@ class MongoDbStorageDriverTest(FunctionalTest):
         assert not driver.is_finalized(project_id, vault_id, file_id)
 
     def test_block_crud(self):
-        driver = MongoDbStorageDriver()
+        driver = MongoDbStorageDriver(self.conn)
 
         project_id = 'project_id'
         vault_id = 'vault_id'
@@ -94,7 +125,7 @@ class MongoDbStorageDriverTest(FunctionalTest):
 
     def test_file_assignment(self):
 
-        driver = MongoDbStorageDriver()
+        driver = MongoDbStorageDriver(self.conn)
 
         project_id = 'project_id'
         vault_id = 'vault_id'
@@ -165,3 +196,102 @@ class MongoDbStorageDriverTest(FunctionalTest):
         gen = driver.create_block_generator(project_id, vault_id)
 
         fetched_blocks = list(gen)
+
+
+'''
+http://blogs.skicelab.com/maurizio/python-unit-testing-and-mongodb.html
+Author/Credit: Posted by Maurizio Sambati Wed 03 October 2012
+'''
+
+
+class MongoTemporaryInstance(object):  # pragma: no cover
+    """Singleton to manage a temporary MongoDB instance
+
+    Use this for testing purpose only. The instance is automatically destroyed
+    at the end of the program.
+
+    """
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+            atexit.register(cls._instance.shutdown)
+        return cls._instance
+
+    def __init__(self):
+        self._port = 8888
+        self._url = "127.0.0.1"
+        self._tmpdir = tempfile.mkdtemp()
+        self._process = subprocess.Popen(['mongod', '--bind_ip', 'localhost',
+                                          '--port', str(self._port),
+                                          '--dbpath', self._tmpdir,
+                                          '--nojournal', '--nohttpinterface',
+                                          '--noauth', '--smallfiles',
+                                          '--syncdelay', '0',
+                                          '--maxConns', '10',
+                                          '--nssize', '1', ],
+                                         stdout=open(os.devnull, 'wb'),
+                                         stderr=subprocess.STDOUT)
+
+        # XXX: wait for the instance to be ready
+        #      Mongo is ready in a glance, we just wait to be able to open a
+        #      Connection.
+        for i in range(3):
+            time.sleep(0.1)
+            try:
+                self._conn = MongoClient(self._url, self._port)
+                #self._conn = pymongo.Connection('localhost', int(self._port))
+            except pymongo.errors.ConnectionFailure:
+                continue
+            else:
+                break
+        else:
+            self.shutdown()
+            assert False, 'Cannot connect to the mongodb test instance'
+
+    @property
+    def conn(self):
+        return self._conn
+
+    def shutdown(self):
+        if self._process:
+            self._process.terminate()
+            self._process.wait()
+            self._process = None
+            shutil.rmtree(self._tmpdir, ignore_errors=True)
+
+
+'''
+KEEP as usage reference... 
+
+http://blogs.skicelab.com/maurizio/python-unit-testing-and-mongodb.html
+Author/Credit: Posted by Maurizio Sambati Wed 03 October 2012
+class TestCase(unittest.TestCase):
+    """TestCase with an embedded MongoDB temporary instance.
+
+    Each test runs on a temporary instance of MongoDB. Please note that
+    these tests are not thread-safe and different processes should set a
+    different value for the listening port of the MongoDB instance with the
+    settings `global.MONGODB_TEST_PORT`.
+
+    A test can access the connection using the attribute `conn`.
+
+    """
+    fixtures = []
+
+    def __init__(self, *args, **kwargs):
+        super(TestCase, self).__init__(*args, **kwargs)
+        self.db = MongoTemporaryInstance.get_instance()
+        self.conn = self.db.conn
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+
+        for db_name in self.conn.database_names():
+            self.conn.drop_database(db_name)
+
+        for fixture in self.fixtures:
+            load_fixture(self.conn, fixture)
+'''
