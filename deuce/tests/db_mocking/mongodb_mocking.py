21aba8941097f3490d80b394aab8ef370a2fa469
raxyu <xuanyu1@yahoo.com>
2014-05-28 12:07:05 -0400

    CBDEUCE-54: mongo drv use sqlite ref test: mongo mocking aggragate returns more query results

--- deuce/tests/db_mocking/mongodb_mocking.py
+++ deuce/tests/db_mocking/mongodb_mocking.py
@@ -11,24 +11,33 @@ from mongomock.collection import Collection
 
 # Mongomock does not have aggregate impl.
 # Patch a quick impl.
-def patch_aggregate(self, list):
-    if self._aggregate_calling_cnt is 0:
-        retval = dict()
-        retval['result'] = [{'blocks': [{'blockid': 'full mocking'}]}]
-        self._aggregate_calling_cnt += 1
-        return retval
-    else:
+def patch_aggregate(self, arglist):
+    num_blocks = 0
+    for item in arglist:
+        if any(key == '$limit' for key in item):
+            num_blocks = item['$limit']
+
+    if num_blocks == -1:
         retval = dict()
         retval['result'] = [{'blocks': []}]
-        self._aggregate_calling_cnt += 1
         return retval
+
+    if num_blocks != 4:
+        num_blocks = 40
+
+    retval = dict()
+
+    blocks = [{'blockid': 'block_{0}'.format(x),
+        'offset': x * 333} for x in range(0, num_blocks)]
+    retval['result'] = [{'blocks': blocks}]
+
+    return retval
 
 old_init = Collection.__init__
 
 
 def new_init(self, db, name):
     old_init(self, db, name)
-    self._aggregate_calling_cnt = 0
 
 Collection.aggregate = patch_aggregate
 Collection.__init__ = new_init
