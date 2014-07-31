3578bc9716d1a8e20754da50e54fad10c9c3718f
BenjamenMeyer <ben.meyer@rackspace.com>
2014-07-31 18:04:55 +0000

    Get File Wrong Content-Type Bug Fix
    JIRA: CBDEUCE-99
    - Enhancement: Added test for auth token to the root controller
    - Enhancement: Made explicit the test for the project-id on the root controller

--- deuce/hooks/authtokenhook.py
+++ deuce/hooks/authtokenhook.py
@@ -3,7 +3,7 @@ from pecan.hooks import PecanHook
 from pecan.core import abort
 
 
-class ProjectIDHook(PecanHook):
+class AuthTokenHook(PecanHook):
     """Every request that hits Deuce must have a header specifying the
     project id that the request is for. The Project ID is synonymous with
     Account ID, Mosso ID, etc. in the Rackspace world.
@@ -16,9 +16,9 @@ class ProjectIDHook(PecanHook):
         # Enforce the existence of the x-project-id header and assign
         # the value to the request project id.
         try:
-            state.request.project_id = state.request.headers['x-project-id']
+            state.request.auth_token = state.request.headers['x-auth-token']
             # TODO: validate the project_id
         except KeyError:
             # Invalid request
-            abort(400, comment="Missing Header : X-Project-ID",
+            abort(401, comment="Missing Header : X-Auth-Token",
                   headers={'Transaction-ID': state.request.context.request_id})
