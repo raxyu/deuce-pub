38df6c00c49bfa572e904553f205bd44b339a159
BenjamenMeyer <ben.meyer@rackspace.com>
2014-08-11 21:13:58 +0000

    Disk Driver Vault Deletion Fix
    JIRA: CBDEUCE-129
    - Enhancement: Use the create_project_id() instead of a static version
    - Enhancement: Added a tests for the vault deletion to ensure we don't delete a non-empty vault

--- deuce/tests/test_vault_controller.py
+++ deuce/tests/test_vault_controller.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 from webtest import TestApp
 from deuce.tests import FunctionalTest
 import os
+import hashlib
+from random import randrange
 
 import json
 
@@ -10,7 +12,7 @@ class TestVaultController(FunctionalTest):
 
     def setUp(self):
         super(TestVaultController, self).setUp()
-        self._hdrs = {"x-project-id": 'testvaultctrl',
+        self._hdrs = {"x-project-id": self.create_project_id(),
             "x-auth-token": ''}
 
     def test_vault_leaf(self):
@@ -30,6 +32,67 @@ class TestVaultController(FunctionalTest):
         response = self.app.get(vault_path, headers=self._hdrs,
                 expect_errors=True)
         self.assertEqual(response.status_int, 404)
+
+    def test_vault_deletion(self):
+
+        # 1. Delete non-existent vault
+        vault_name = self.create_vault_id()
+        vault_path = '/v1.0/{0}'.format(vault_name)
+        response =  self.app.delete(vault_path,
+            headers = self._hdrs, expect_errors=True)
+        self.assertEqual(response.status_code, 404)
+
+        # 2. Create Vault and Delete it (Empty Vault)
+        vault_name = self.create_vault_id()
+        vault_path = '/v1.0/{0}'.format(vault_name)
+        response =  self.app.put(vault_path,
+            headers = self._hdrs)
+        self.assertEqual(response.status_code, 201)
+
+        response = self.app.delete(vault_path,
+            headers = self._hdrs)
+        self.assertEqual(response.status_code, 204)
+
+        # 3. Create Vault, Add a Block, and Delete It (Non-Empty Vault)
+        vault_name = self.create_vault_id()
+        vault_path = '/v1.0/{0}'.format(vault_name)
+        response =  self.app.put(vault_path,
+            headers = self._hdrs)
+        self.assertEqual(response.status_code, 201)
+
+        # Build a dummy block
+        block_data = os.urandom(randrange(1,2000))
+        block_hash = hashlib.sha1()
+        block_hash.update(block_data)
+        block_id = block_hash.hexdigest()
+        block_path = '{0:}/blocks/{1:}'.format(vault_path, block_id)
+
+        # Upload a dummy block
+        headers = {}
+        for k, v in self._hdrs.items():
+            headers[k] = v
+
+        headers['content-type'] = 'application/binary'
+        headers['content-length'] = str(len(block_data))
+
+        response = self.app.put(block_path, headers=headers,
+            params=block_data)
+        self.assertEqual(response.status_code, 201)
+        
+        # Delete the vault
+        response = self.app.delete(vault_path,
+            headers = self._hdrs, expect_errors=True)
+        self.assertEqual(response.status_code, 412)
+
+        # Delete the dummy block
+        # TODO:
+        #response = self.app.delete(block_path, headers=self._hdrs)
+        #self.assertEqual(response.status_code, 204)
+        #
+        ## Delete the vault
+        #response = self.app.delete(vault_path,
+        #    headers = self._hdrs)
+        #self.assertEqual(response.status_code, 204)
 
     def test_vault_crud(self):
         vault_name = self.create_vault_id()
