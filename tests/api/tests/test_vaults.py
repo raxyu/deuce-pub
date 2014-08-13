a9cc78d3222bc49c563ac674c0a7066d89de5800
Jorge Chai <jorge.chai@rackspace.com>
2014-08-13 16:14:58 -0400

    Update GET/HEAD Vault tests

--- tests/api/tests/test_vaults.py
+++ tests/api/tests/test_vaults.py
@@ -37,13 +37,35 @@ class TestEmptyVault(base.TestBase):
         """Get an individual vault"""
 
         resp = self.client.get_vault(self.vaultname)
-        self.assertEqual(resp.status_code, 204,
+        self.assertEqual(resp.status_code, 200,
                          'Status code returned for Get Vault: {0} . '
-                         'Expected 204'.format(resp.status_code))
+                         'Expected 200'.format(resp.status_code))
         self.assertHeaders(resp.headers)
-        self.assertEqual(len(resp.content), 0,
-                         'Response Content was not empty. Content: '
-                         '{0}'.format(resp.content))
+        resp_body = resp.json()
+        self.assertIn('storage', resp_body)
+        self.assertIn('metadata', resp_body)
+
+        storage = resp_body['storage']
+        self.assertEqual(storage['block-count'], 0)
+        self.assertEqual(storage['internal'], {})
+        self.assertEqual(storage['total-size'], 0)
+
+        meta = resp_body['metadata']
+        self.assertIn('files', meta)
+        self.assertIn('internal', meta)
+        self.assertIn('blocks', meta)
+        self.assertIn('file-blocks', meta)
+
+        meta_files = meta['files']
+        self.assertEqual(meta_files['count'], 0)
+
+        self.assertEqual(meta['internal'], {})
+
+        meta_blocks = meta['blocks']
+        self.assertEqual(meta_blocks['count'], 0)
+
+        meta_file_blocks = meta['file-blocks']
+        self.assertEqual(meta_file_blocks['count'], 0)
 
     def test_delete_vault(self):
         """Delete a Vault"""
@@ -60,21 +82,14 @@ class TestEmptyVault(base.TestBase):
     def test_vault_head(self):
         """Head of an individual vault. Get the statistics for a vault"""
 
-        # TODO
-        self.skipTest('HEAD Vault functionality not implemented')
         resp = self.client.vault_head(self.vaultname)
-        self.assertEqual(resp.status_code, 200,
+        self.assertEqual(resp.status_code, 204,
                          'Status code returned for Vault HEAD: {0} . '
-                         'Expected 200'.format(resp.status_code))
+                         'Expected 204'.format(resp.status_code))
         self.assertHeaders(resp.headers)
-        self.assertIn('X-File-Count', resp.headers)
-        self.assertEqual(resp.headers['X-File-Count'], 0)
-        self.assertIn('X-Unfinalized-Count', resp.headers)
-        self.assertEqual(resp.headers['X-Unfinalized-Count'], 0)
-        self.assertIn('X-Block-Count', resp.headers)
-        self.assertEqual(resp.headers['X-Block-Count'], 0)
-        self.assertIn('X-Byte-Count', resp.headers)
-        self.assertEqual(resp.headers['X-Byte-Count'], 0)
+        self.assertEqual(len(resp.content), 0,
+                         'Response Content was not empty. Content: '
+                         '{0}'.format(resp.content))
 
     def tearDown(self):
         super(TestEmptyVault, self).tearDown()
