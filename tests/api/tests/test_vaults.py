60e3ed6f3487e7635a639bdcfc205d9255d92b8f
Jorge Chai <jorge.chai@rackspace.com>
2014-08-14 14:10:34 -0400

    Test vault statistics for a populated vault

--- tests/api/tests/test_vaults.py
+++ tests/api/tests/test_vaults.py
@@ -85,7 +85,54 @@ class TestVaultWithBlocksFiles(base.TestBase):
 
     def setUp(self):
         super(TestVaultWithBlocksFiles, self).setUp()
+        self.create_empty_vault()
+        [self.upload_block() for _ in range(20)]
+        [self.create_new_file() for _ in range(3)]
+        # Assign specific blocks to the files created
+        # Assign 3 unique blocks to file 1
+        self.assign_blocks_to_file(blocks=[0, 1, 2],
+                                   file_url=self.files[0].Url)
+
+        # Assign 5 blocks to file 2, sharing 2 blocks with file 1
+        self.assign_blocks_to_file(blocks=[4, 5, 0, 6, 2],
+                                   file_url=self.files[1].Url)
+
+        # Assign 8 unique blocks to file 3 and finalize it
+        self.assign_blocks_to_file(blocks=range(10, 18),
+                                   file_url=self.files[2].Url)
+        self.finalize_file(file_url=self.files[2].Url)
+        # 14 blocks have been assigned to 3 files
+
+    def test_get_populated_vault(self):
+        """Get the statistics of a populated vault"""
+
+        resp = self.client.get_vault(self.vaultname)
+        self.assertEqual(resp.status_code, 200,
+                         'Status code returned for Get Vault: {0} . '
+                         'Expected 200'.format(resp.status_code))
+        self.assertHeaders(resp.headers, json=True)
+        resp_body = resp.json()
+        storage = resp_body['storage']
+        self.assertEqual(storage['block-count'], 20)
+        self.assertEqual(storage['internal'], {})
+        self.assertEqual(storage['total-size'], 30720 * 20)
+
+        meta = resp_body['metadata']
+        meta_files = meta['files']
+        self.assertEqual(meta_files['count'], 3)
+
+        self.assertEqual(meta['internal'], {})
+
+        meta_blocks = meta['blocks']
+        self.assertEqual(meta_blocks['count'], 20)
+
+        meta_file_blocks = meta['file-blocks']
+        self.assertEqual(meta_file_blocks['count'], 14)
 
     def tearDown(self):
         super(TestVaultWithBlocksFiles, self).tearDown()
+        [self.client.delete_file(vaultname=self.vaultname,
+            fileid=fileid.Id) for fileid in self.files]
+        [self.client.delete_block(self.vaultname, block.Id) for block in
+            self.blocks]
         self.client.delete_vault(self.vaultname)
