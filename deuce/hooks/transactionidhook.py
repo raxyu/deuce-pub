a774ee65bbed961d44a53db953b65f3495f0f464
BenjamenMeyer <ben.meyer@rackspace.com>
2014-08-14 20:12:49 +0000

    Deuce Context - Transaction-ID
    - Update the standard response headers with the Transaction-ID

--- deuce/hooks/transactionidhook.py
+++ deuce/hooks/transactionidhook.py
@@ -5,9 +5,13 @@ from deuce.common import context
 
 import deuce
 
+
 class TransactionIDHook(PecanHook):
     def on_route(self, state):
 
         transaction = context.RequestContext()
         setattr(local.store, 'context', transaction)
         deuce.context.request_id = transaction
+
+        from pecan import response
+        response.headers['X-Transaction-ID'] = deuce.context.request_id
