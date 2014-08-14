8665aabe360a77493adc31dc89b9b04086188db0
BenjamenMeyer <ben.meyer@rackspace.com>
2014-08-14 17:48:56 +0000

    Deuce Context - Project ID
    JIRA: CBDEUCE-148
    
    - Set the Project ID to the Context Object instead of the Request Object

--- deuce/hooks/projectidhook.py
+++ deuce/hooks/projectidhook.py
@@ -2,11 +2,12 @@
 from pecan.hooks import PecanHook
 from pecan.core import abort
 
+import deuce
 
 class ProjectIDHook(PecanHook):
     """Every request that hits Deuce must have a header specifying the
     project id that the request is for. The Project ID is synonymous with
-    Account ID, Mosso ID, etc. in the Rackspace world.
+    Account ID, Tenant ID, etc. in the Rackspace world.
 
     If a request does not provide the header the request should fail
     with a 400"""
@@ -16,7 +17,7 @@ class ProjectIDHook(PecanHook):
         # Enforce the existence of the x-project-id header and assign
         # the value to the request project id.
         try:
-            state.request.project_id = state.request.headers['x-project-id']
+            deuce.context.project_id = state.request.headers['x-project-id']
             # TODO: validate the project_id
         except KeyError:
             # Invalid request
