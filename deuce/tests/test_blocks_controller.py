b8e7d52ff656f62f7750f72fe4f35a841fd75190
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2014-08-19 12:59:28 -0400

    fix:application/binary to application/octet-stream

--- deuce/tests/test_blocks_controller.py
+++ deuce/tests/test_blocks_controller.py
@@ -249,7 +249,7 @@ class TestBlocksController(FunctionalTest):
                 # NOTE: Very important to set the content-type
                 # header. Otherwise pecan tries to do a UTF-8 test.
                 headers = {
-                    "Content-Type": "application/binary",
+                    "Content-Type": "application/octet-stream",
                     "Content-Length": str(size),
                 }
 
