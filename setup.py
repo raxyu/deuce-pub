50aac57ca81bd4e1b0ee0ee3acb4813611f5bb52
xyu <xuan.yu@rackspace.com>
2014-08-19 11:39:01 -0400

    156_wheel_inst: move config.py to ./bin, so it's in a fixed location for OP, then public/ and templates have to move along as well

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 try:
-    from setuptools import setup, find_packages
+    from distutils.core import setup
+    from setuptools import find_packages
 except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
@@ -13,14 +14,22 @@ setup(
     name='deuce',
     version='0.1',
     description='Deuce - Block-level de-duplication as-a-service',
+    license='',
+    url='',
     author='Rackspace',
     author_email='',
+    include_package_data=True,
     install_requires=REQUIRES,
     test_suite='deuce',
     zip_safe=False,
-    include_package_data=True,
-    package_data={
-        'deuce': ['public/css/style.css', 'public/images/logo.png'],
-    },
-    packages=find_packages(exclude=['tests', 'deuce/tests'])
+    data_files=[('bin', ['config.py']),
+                ('bin/templates',
+                    ['deuce/templates/error.html',
+                    'deuce/templates/index.html',
+                    'deuce/templates/layout.html']),
+                ('bin/public/css',
+                    ['deuce/public/css/style.css']),
+                ('bin/public/images',
+                    ['deuce/public/images/logo.png'])],
+    packages=find_packages(exclude=['tests'])
 )
