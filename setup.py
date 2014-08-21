2269ef3d107488034e46d4748ac12460452590cc
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2014-08-21 11:44:07 -0400

    fix: setup.py
    REQUIRES is now extensible based on python version

--- setup.py
+++ setup.py
@@ -7,26 +7,26 @@ except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages
-if sys.version_info[0] == 3:
-    REQUIRES = ['six', 'pecan', 'setuptools >= 1.1.6',
-    'cassandra-driver', 'pymongo', 'msgpack-python', 'asyncio',
-    'aiohttp', 'python-swiftclient']
 else:
     REQUIRES = ['six', 'pecan', 'setuptools >= 1.1.6',
-    'cassandra-driver', 'pymongo', 'msgpack-python', 'gevent',
-    'python-swiftclient']
-setup(
-    name='deuce',
-    version='0.1',
-    description='Deuce - Block-level de-duplication as-a-service',
-    license='Apache License 2.0',
-    url='github.com/rackerlabs/deuce',
-    author='Rackspace',
-    author_email='',
-    include_package_data=True,
-    install_requires=REQUIRES,
-    test_suite='deuce',
-    zip_safe=False,
-    data_files=[('bin', ['config.py'])],
-    packages=find_packages(exclude=['tests'])
-)
+                'cassandra-driver', 'pymongo', 'msgpack-python',
+                'python-swiftclient']
+    if sys.version_info[0] == 3:
+        REQUIRES.extend(['asyncio', 'aiohttp'])
+    else:
+        REQUIRES = ['gevent']
+    setup(
+        name='deuce',
+        version='0.1',
+        description='Deuce - Block-level de-duplication as-a-service',
+        license='Apache License 2.0',
+        url='github.com/rackerlabs/deuce',
+        author='Rackspace',
+        author_email='',
+        include_package_data=True,
+        install_requires=REQUIRES,
+        test_suite='deuce',
+        zip_safe=False,
+        data_files=[('bin', ['config.py'])],
+        packages=find_packages(exclude=['tests'])
+    )
